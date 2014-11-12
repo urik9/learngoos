514b2f042642be3f9f47fca9739797a185b86bc9
urik9 <uri@wix.com>
2014-11-12 13:35:31 +0200

    2 e2e pass post refactor

--- src/main/scala/AuctionMessageTranslator.scala
+++ src/main/scala/AuctionMessageTranslator.scala
@@ -2,38 +2,49 @@ import java.util.EventListener
 
 import org.jivesoftware.smack.packet.Message
 import org.jivesoftware.smack.{Chat, MessageListener}
-
-
 /**
  * Created by Uri_Keinan on 11/3/14.
  */
+
+
 class AuctionMessageTranslator(listener: AuctionEventListener) extends MessageListener {
-  val event = scala.collection.mutable.Map[String, String]()
 
   def processMessage(chat: Chat, message: Message) = {
-    getMessageParams(message)
-
-    val eventType:String = event.get("Event").get
-    if (eventType equals "CLOSE") {
+    val event = AuctionEvent.from(message.getBody)
+    if (event.eventType equals "CLOSE") {
       listener.auctionClosed()
-    } else if (eventType equals "PRICE") {
-      listener.currentPrice(Integer.parseInt(event.get("CurrentPrice").get), Integer.parseInt(event.get("Increment").get))
+    } else if (event.eventType equals "PRICE") {
+      listener.currentPrice (event.currentPrice(), event.increment())
     }
-
-    //
-    //      case "CLOSE" => listener.auctionClosed()
-    //      case "PRICE" => listener.currentPrice(Integer.parseInt(event.get("CurrentPrice").toString),Integer.parseInt(event.get("Increment").toString))
   }
 
+  class AuctionEvent{
+    val fields = scala.collection.mutable.Map[String, String]()
+    def eventType():String = {get("Event")}
+    def currentPrice(): Int = {getInt("CurrentPrice")}
+    def increment(): Int = {getInt("Increment")}
 
-  def getMessageParams(message: Message) = {
-    val fields: Array[String] = message.getBody.split(";")
-    for (field <- fields) {
+    def getInt(fieldName: String) = {get(fieldName).toInt}
+    def get(fieldName: String) = {fields.get(fieldName).get}
+
+
+
+    def addField(field: String){
       val pair: Array[String] = field.split(":")
-      event += pair(0).trim -> pair(1).trim
+      fields += pair(0).trim ->  pair(1).trim
     }
-    event
   }
+
+  object AuctionEvent{
+    def from(messageBody: String) = {
+      val event: AuctionEvent = new AuctionEvent()
+      messageBody.split(";").foreach(field => event.addField(field))
+      event
+    }
+  }
+
+
+
 
 }
 
